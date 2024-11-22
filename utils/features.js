8b3d8f9e50b162609b1502571f1a22b7346adf54
CyKrome007 <OwaizKhan1111@gmail.com>
2024-11-22 20:19:50 +0530

    Changed cookieOptions: sameSite: false => 'none'

--- utils/features.js
+++ utils/features.js
@@ -6,7 +6,7 @@ import {getBase64, getSockets} from "../lib/helper.js";
 
 const cookieOptions = {
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    sameSite: false,
+    sameSite: 'none',
     httpOnly: true,
     secure: true,
 };
